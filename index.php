28131fa5af997b127c725f3e672ab990281de12d
miyang0 <chuyyanaka@gmail.com>
2026-04-01 14:39:02 +0200

    updated index and css

--- index.php
+++ index.php
@@ -9,13 +9,27 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>VisualEyes</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
 
 <nav>
     <?php require_once "blocks/nav.php"; ?>
 </nav>
+
+<header>
+    <h1></h1>
+    <p>Simple and accessible - we created the VisualEyes app to make shopping more accessible for people with visual
+        impairments,
+        especially when looking for clothing. With the color detector app, you can get information about a product by
+        pointing your
+        camera at it and tapping “Start.” It will describe the colours and patterns seen on the product.
+        Furthermore, you can use the app to search for a specific colour: select the colour or colours you want to
+        search for and then scan your surroundings.
+        You can also mark colours as favourites to find them more quickly.
+    </p>
+</header>
 
 </body>
 </html>
