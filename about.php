adf5b3cce67d4e24eb0c7b8aff090196421d11f5
DaniHaruu <danielleruwaard8@gmail.com>
2026-04-01 11:59:13 +0200

    About us pagina html

--- about.php
+++ about.php
@@ -12,6 +12,43 @@
     <title>Document</title>
 </head>
 <body>
+<h1>About us</h1>
+<p>We are Seth, Miya, Alex, and Daniëlle; 4 students from a university called Hogeschool Rotterdam who were tasked with
+    designing something that could make shopping more pleasant for someone with a visual impairment. After much
+    brainstorming, creating prototypes, and interviewing the target group, we arrived at the final product:
+    ‘VisualEyes’.</p>
+
+<h2>Goals</h2>
+<p>In addition to wanting to design something helpful for people with a visual impairment, we also wanted to apply what
+    we had learned during the last period and work on a few learning objectives. During the last period, we worked more
+    with JavaScript, learned to design ecologically, and learned more about what it is like to have a disability. The
+    learning objectives we wanted to achieve this period are based on entrepeneurship and involvement. These are all the
+    learning objectives:</p>
+<ul>
+    <li>Entrepreneurship: Communication; I can deliver presentations and documentation tailored to the target audience
+        and/or client, in which relevant information is bundled and visualized.
+    </li>
+    <li>Entrepreneurship: Project-based work; I can estimate deliverables and deadlines, and I am able to estimate the
+        entire process and adhere to it, so that the client knows what to expect.
+    </li>
+    <li>Entrepreneurship: Entrepreneurial attitude; I make an active contribution to the team.</li>
+    <li>Entrepreneurship: Entrepreneurial skills; I expand my professional knowledge by attending an industry-related
+        networking event.
+    </li>
+    <li>Involvement: Reflective capacity; I want to develop myself and, to that end, I look critically at myself, am
+        open to feedback, and identify, substantiate, and work on areas for improvement.
+    </li>
+    <li>Involvement: Responsibility; I am aware of my own qualities and the context in which I work and therefore make
+        conscious choices within the project.
+    </li>
+</ul>
+
+<h2>Reasons</h2>
+<p>Originally, this was an assignment we were given by our study program. Each group received one random target group
+    and one random activity to improve for this target group. We were assigned people with visual impairments and
+    shopping. But we really do want to help people. At this moment, there are probably already better assistive devices
+    available than what we can create in such a short time. But we are learning from this. Because we are doing this
+    now, we can make something even better in the future.</p>
 
 <nav>
     <?php require_once "blocks/nav.php"; ?>
