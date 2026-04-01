dac09c3eac54cc5d76569dfe4bbe91ffa0b9c0b8
DaniHaruu <danielleruwaard8@gmail.com>
2026-04-01 13:39:15 +0200

    homepage

---
+++ homepage.php
@@ -0,0 +1,28 @@
+<?php
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<h1>Homepage</h1>
+<p>Welcome to our page! We created the VisualEyes app to make shopping more pleasant for people with a visual
+    impairment, particularly when searching for clothing. With the VisualEyes app, you can get information about a
+    product by pointing your camera at it and clicking on start. It will then tell you the color of the product and the
+    type of pattern on it, if there is one. Furthermore, you can also use the app to search for a specific color. You
+    select the color or colors you want to search for and then scan the surroundings. You can also mark colors as
+    favorites to find them more quickly.</p>
+
+<nav>
+    <?php require_once "blocks/nav.php"; ?>
+</nav>
+
+</body>
+</html>
